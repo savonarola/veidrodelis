ba0646abbf39006a9aafd9651abb5d1e92fce3db
Ilia Averianov <av@rubybox.dev>
2026-02-07 23:28:03 +0200

    Bump version: 0.1.3 → 0.1.4

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Veidrodelis.MixProject do
   use Mix.Project
 
-  @version "0.1.3"
+  @version "0.1.4"
   @github_url "https://github.com/savonarola/veidrodelis"
   @doc_extras [
     "doc/about.md",
